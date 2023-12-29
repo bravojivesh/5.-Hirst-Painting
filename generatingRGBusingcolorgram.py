da865b038f018fc97003cb6d5a73396c30240943
JIVESH <bravojivesh@gmail.com>
2023-12-29 22:11:13 +0530

    final version. Creates 10 by 10 sqyare of dots

---
+++ generatingRGBusingcolorgram.py
@@ -0,0 +1,27 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+# import colorgram
+## colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     rgb_color1= color.rgb.r #red
+#     rgb_color2= color.rgb.g #green
+#     rgb_color3= color.rgb.b #blue
+#     rgb_colors.append((rgb_color1,rgb_color2, rgb_color3)) #note: this will first create a tuple. And then a list of
+    #tuples.
+    # rgb_colors.append(color.rgb)
+    #Commenting the above as we do not want so much of computing needed. The RGB color code are as follows.
+import turtle as tu
+
+rgb_colors= [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+tim=tu.Turtle()
+screen= tu.Screen()
+
+tim.shape("circle")
+screen.exitonclick()
+
+tim.forward(100)
+
+# print(rgb_colors)
+# print (type((rgb_colors)[0])) #to check I created the tuple properly
+
